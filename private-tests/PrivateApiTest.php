58a843f9eaad142fad70cf4ba547b7292a5b7c02
Nic Rosental <nic@epiclabs.com>
2018-01-29 12:59:44 -0500

    moved VCR statements to setUp/tearDown

--- private-tests/PrivateApiTest.php
+++ private-tests/PrivateApiTest.php
@@ -6,41 +6,36 @@ use VCR\VCR;
 
 class PrivateApiTest extends TestCase
 {
+    protected function setUp() {
+        VCR::configure()->setCassettePath('private-tests/fixtures');
+        VCR::turnOn();
+    }
+
+    protected function tearDown() {
+        VCR::eject();
+        VCR::turnOff();
+    }
 
     public function test_it_makes_a_call_to_accounts() {
-
-      VCR::configure()->setCassettePath('private-tests/fixtures');
-      VCR::turnOn();
       VCR::insertCassette('accounts_endpoint.yml');
 
       $this->privateApi = new Cointrader\PrivateApi(new Cointrader\ApiCaller, API_KEY, API_SECRET, API_PASSPHRASE);
-
       $accounts = $this->privateApi->tradingAccounts();
-
-      VCR::eject();
-      VCR::turnOff();
 
       $this->assertTrue(is_array($accounts));
     }
 
     public function test_it_makes_a_call_to_account_holds() {
-      VCR::configure()->setCassettePath('private-tests/fixtures');
-      VCR::turnOn();
       VCR::insertCassette('account_holds_endpoint.yml');
 
       $this->privateApi = new Cointrader\PrivateApi(new Cointrader\ApiCaller, API_KEY, API_SECRET, API_PASSPHRASE);
 
       $products = $this->privateApi->accountHolds(USD_ACCOUNT);
 
-      VCR::eject();
-      VCR::turnOff();
-
       $this->assertTrue(is_array($products));
     }
 
     public function test_it_places_a_buy_limit_order() {
-        VCR::configure()->setCassettePath('private-tests/fixtures');
-        VCR::turnOn();
         VCR::insertCassette('account_place_order_endpoint.yml');
 
         $this->privateApi = new Cointrader\PrivateApi(new Cointrader\ApiCaller, API_KEY, API_SECRET, API_PASSPHRASE);
@@ -56,9 +51,6 @@ class PrivateApiTest extends TestCase
 
         $order = $this->privateApi->placeOrder($orderParams);
 
-        VCR::eject();
-        VCR::turnOff();
-
         $this->assertTrue(is_array($order));
 
         return $order['id'];
@@ -70,16 +62,11 @@ class PrivateApiTest extends TestCase
      */
 
     public function test_it_gets_order_info() {
-        VCR::configure()->setCassettePath('private-tests/fixtures');
-        VCR::turnOn();
         VCR::insertCassette('account_get_order_info_endpoint.yml');
 
         $this->privateApi = new Cointrader\PrivateApi(new Cointrader\ApiCaller, API_KEY, API_SECRET, API_PASSPHRASE);
 
         $order = $this->privateApi->order($orderId);
-
-        VCR::eject();
-        VCR::turnOff();
 
         $this->assertTrue(is_array($order));
     }
@@ -89,31 +76,25 @@ class PrivateApiTest extends TestCase
      */
 
     public function test_it_cancels_an_order($orderId) {
-        VCR::configure()->setCassettePath('private-tests/fixtures');
-        VCR::turnOn();
         VCR::insertCassette('account_delete_order_endpoint.yml');
 
         $this->privateApi = new Cointrader\PrivateApi(new Cointrader\ApiCaller, API_KEY, API_SECRET, API_PASSPHRASE);
 
         $order = $this->privateApi->cancelOrder($orderId);
 
-        VCR::eject();
-        VCR::turnOff();
+
 
         $this->assertTrue(is_array($order));
     }
 
     public function test_it_cancels_all_eth_orders() {
-        VCR::configure()->setCassettePath('private-tests/fixtures');
-        VCR::turnOn();
         VCR::insertCassette('account_delete_all_eth_orders_endpoint.yml');
 
         $this->privateApi = new Cointrader\PrivateApi(new Cointrader\ApiCaller, API_KEY, API_SECRET, API_PASSPHRASE);
 
         $order = $this->privateApi->cancelAllOrders('ETH-USD');
 
-        VCR::eject();
-        VCR::turnOff();
+
 
         $this->assertTrue(is_array($order));
     }

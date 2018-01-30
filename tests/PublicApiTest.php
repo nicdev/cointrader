5f70c77881e0bf949ee9f93ac3f0ee7683a9bceb
Nic Rosental <nic@epiclabs.com>
2018-01-29 21:36:17 -0500

    renamed tests, and reformatted

--- tests/PublicApiTest.php
+++ tests/PublicApiTest.php
@@ -1,42 +1,46 @@
 <?php
 
+namespace CointraderTest;
+
 use PHPUnit\Framework\TestCase;
 use Cointrader\ApiCaller;
+use Cointrader\PublicApi;
 use VCR\VCR;
 
 class PublicApiTest extends TestCase
 {
 
-    public function test_it_makes_a_call_to_products() {
+    public function testGetsProducts()
+    {
 
-      VCR::turnOn();
-      VCR::insertCassette('products_endpoint.yml');
+        VCR::turnOn();
+        VCR::insertCassette('products_endpoint.yml');
 
-      $this->publicApi = new Cointrader\PublicApi(new ApiCaller);
+        $this->publicApi = new PublicApi(new ApiCaller);
 
-      $products = $this->publicApi->products();
+        $products = $this->publicApi->products();
 
-      VCR::eject();
-      VCR::turnOff();
+        VCR::eject();
+        VCR::turnOff();
 
-      $this->assertTrue(is_array($products));
+        $this->assertTrue(is_array($products));
     }
 
-    public function test_it_makes_a_call_to_order_book() {
-      VCR::turnOn();
-      VCR::insertCassette('orderbook_endpoint.yml');
+    public function testGetsOrderBook()
+    {
+        VCR::turnOn();
+        VCR::insertCassette('orderbook_endpoint.yml');
 
-      $this->publicApi = new Cointrader\PublicApi(new ApiCaller);
+        $this->publicApi = new PublicApi(new ApiCaller);
 
-      $orderBook = $this->publicApi->orderBook('BTC-USD', 1);
+        $orderBook = $this->publicApi->orderBook('BTC-USD', 1);
 
-      VCR::eject();
-      VCR::turnOff();
+        VCR::eject();
+        VCR::turnOff();
 
-      $this->assertTrue(is_array($orderBook));
-      $this->assertArrayHasKey('sequence', $orderBook);
-      $this->assertArrayHasKey('bids', $orderBook);
-      $this->assertArrayHasKey('asks', $orderBook);
+        $this->assertTrue(is_array($orderBook));
+        $this->assertArrayHasKey('sequence', $orderBook);
+        $this->assertArrayHasKey('bids', $orderBook);
+        $this->assertArrayHasKey('asks', $orderBook);
     }
-
-  }
+}
